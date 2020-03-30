12ac1784afd5e1017f7fd69db4a3f3c29826c750
徳住友稜 <tokusumi@n2i.jp>
2020-03-30 22:28:50 +0900

    fix db config

--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -4,9 +4,13 @@ from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = os.environ.get('DATABASE_URL')
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
-)
+if SQLALCHEMY_DATABASE_URL.startswith('sqlite'):
+    engine = create_engine(
+        SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
