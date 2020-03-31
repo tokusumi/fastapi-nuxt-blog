03ec060a0da27035674d91187275a8b793268da8
徳住友稜 <tokusumi@n2i.jp>
2020-04-01 03:15:40 +0900

    fix list

--- app/src/pages/list/index.vue
+++ app/src/pages/list/index.vue
@@ -1,44 +1,54 @@
 <template>
-  <v-card class="mx-auto" max-width="1000">
-    <v-container>
-      <v-row dense>
-        <v-col v-for="card in cards" :key="card.title" cols="12" sm="12" md="6">
-          <v-card>
-            <v-img v-if="card.image" :src="card.image" class="white--text align-end" height="300px">
-              <v-card-title class="teal lighten-1" v-text="card.title"></v-card-title>
-            </v-img>
-            <v-img v-else :src="src" class="white--text align-end" height="300px">
-              <v-card-title class="teal lighten-1" v-text="card.title"></v-card-title>
-            </v-img>
-            <v-card-actions>
-              <v-avatar size="35">
-                <img v-if="card.author.icon" :src="card.author.icon" alt="Avatar" />
-                <v-icon v-else>mdi-emoticon</v-icon>
-              </v-avatar>
-              <v-card-text>
-                {{card.author.username}} |
-                <v-icon small>mdi-clock</v-icon>
-                {{card.public_at.substr(0, 10)}}
-              </v-card-text>
-              <v-spacer></v-spacer>
-
-              <v-btn class="teal--text lighten-1" text :to="'/detail/?id=' + card.id">Show...</v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-col>
-      </v-row>
+  <v-app>
+    <v-container class="px-0">
+      <v-card class="mx-auto" max-width="800px" flat>
+        <v-container class="px-0">
+          <v-row dense>
+            <v-col v-for="card in cards" :key="card.title" cols="12" xs="12" sm="6">
+              <v-card>
+                <v-img
+                  v-if="card.image"
+                  :src="card.image"
+                  class="white--text align-end"
+                  min-height="300px"
+                >
+                  <v-card-title class="teal lighten-1 post-title" v-text="card.title"></v-card-title>
+                </v-img>
+                <v-img v-else :src="src" class="white--text align-end" height="300px">
+                  <v-card-title class="teal lighten-1 post-title" v-text="card.title"></v-card-title>
+                </v-img>
+                <v-card-actions>
+                  <v-avatar size="35">
+                    <img v-if="card.author.icon" :src="card.author.icon" alt="Avatar" />
+                    <v-icon v-else>mdi-emoticon</v-icon>
+                  </v-avatar>
+                  <v-card-text>
+                    {{card.author.username}} |
+                    <v-icon small>mdi-clock</v-icon>
+                    {{card.public_at.substr(0, 10)}}
+                  </v-card-text>
+                  <v-spacer></v-spacer>
+                  <v-btn class="teal--text lighten-1" text :to="'/detail/?id=' + card.id">Show...</v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-card>
+          <div class="text-center">
+            <v-pagination
+              color="teal lighten-1"
+              v-model="page"
+              :length="max_page"
+              :total-visible="pagination_len"
+              @input="getPosts"
+              circle
+            ></v-pagination>
+          </div>
+        </v-card>
+      </v-card>
     </v-container>
-    <div class="text-center">
-      <v-pagination
-        color="teal lighten-1"
-        v-model="page"
-        :length="max_page"
-        :total-visible="pagination_len"
-        @input="getPosts"
-        circle
-      ></v-pagination>
-    </div>
-  </v-card>
+  </v-app>
 </template>
 <script>
 export default {
@@ -82,3 +92,8 @@ export default {
   }
 };
 </script>
+<style scoped>
+.post-title {
+  opacity: 0.9;
+}
+</style>
