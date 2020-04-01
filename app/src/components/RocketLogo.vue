14f27b0ebed936b82d2890e6d21be6760eca4e05
T. Tokusumi <41147016+tokusumi@users.noreply.github.com>
2020-04-01 09:35:03 +0900

    Dev (#7)
    * add home
    
    * fix list
    
    * fix profile layout
    
    * fix login
    
    * fix pagination

---
+++ app/src/components/RocketLogo.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="box-rocket" />
+</template>
+
+
+<style scoped>
+.box-rocket {
+  width: 200px;
+  height: 200px;
+  box-sizing: border-box;
+  background: url(https://dev-blog-s3.s3-ap-northeast-1.amazonaws.com/static/home/rocket.svg)
+    no-repeat center;
+}
+
+.VuetifyLogo {
+  width: 180px;
+  transform: rotateY(560deg);
+  animation: turn 3.5s ease-out forwards 1s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
---
+++ app/src/components/WriteLogo.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="box-write" />
+</template>
+
+
+<style scoped>
+.box-write {
+  width: 200px;
+  height: 200px;
+  box-sizing: border-box;
+  background: url(https://dev-blog-s3.s3-ap-northeast-1.amazonaws.com/static/home/design.svg)
+    no-repeat center;
+  background-size: 100% auto;
+  transform: rotateY(560deg);
+  animation: turn 3.5s ease-out forwards 1s;
+}
+
+@keyframes turn {
+  100% {
+    transform: rotateY(0deg);
+  }
+}
+</style>
