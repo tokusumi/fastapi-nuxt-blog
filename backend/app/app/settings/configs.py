b81ffa7d2ab180582ad391599182370b251316bf
T. Tokusumi <41147016+tokusumi@users.noreply.github.com>
2020-05-13 18:15:35 +0900

    Update configs.py

--- backend/app/app/settings/configs.py
+++ backend/app/app/settings/configs.py
@@ -6,8 +6,8 @@ from app.users.models import User
 
 SECRET_KEY = os.getenv("SECRET_KEY", "test")
 JWT_ALGORITHM = "HS256"
-JWT_EXPIRRE_KEY = "exp"
-JWT_ACCESS_TOKEN_EXPIRE_MINUTES = os.getenv('JWt_ACCESS_TOKEN_EXPIRE_MINUTES', 30)
+JWT_EXPIRRE_KEY = os.getenv("JWT_EXPIRREKEY", "exp")
+JWT_ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv('JWT_ACCESS_TOKEN_EXPIRE_MINUTES', 30))
 PWD_CONTEXT = CryptContext(schemes=["bcrypt"], deprecated="auto")
 OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
